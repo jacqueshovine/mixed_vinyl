e6ea39cb5908129f34d6a0d754f363153e703d6d
jacques <hovine.jacques@gmail.com>
2022-06-12 18:04:35 +0200

    add a twig template and return it in the VinylController
    Chapter 8 from Symfony 6 tutorial on Symfonycasts.com

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,17 +2,29 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
 
-class VinylController
+class VinylController extends AbstractController
 {
 
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('Title: PB and Jams');
+        $tracks = [
+            ['song' => 'Song 1', 'artist' => 'Artist 1'],
+            ['song' => 'Song 2', 'artist' => 'Artist 2'],
+            ['song' => 'Song 3', 'artist' => 'Artist 3'],
+            ['song' => 'Song 4', 'artist' => 'Artist 4'],
+        ];
+
+        // The render method returns a Response object
+        return $this->render('vinyl/homepage.html.twig', [
+            'title' => 'PB and Jams',
+            'tracks' => $tracks,
+        ]);
     }
 
     #[Route('/browse/{slug}')]
